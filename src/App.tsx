8c8ebd89970ced96af3b8a91a22f6b54cc985dc1
Jonathan Peraza Feliciano <jonathan.peraza@wizeline.com>
2020-05-02 16:05:52 -0500

    feat: add tailwind config

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Card from './components/card';
+import './assets/main.scss';
+
+function App() {
+  return (
+    <Card />
+  );
+}
+
+export default App;
