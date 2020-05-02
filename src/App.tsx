9eb342361b367bfd90853f2ac8208079941302d7
Jonathan Peraza Feliciano <jonathan.peraza@wizeline.com>
2020-05-02 16:46:06 -0500

    feat: add styles to card and add chip component

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,18 @@ import Card from './components/card';
 import './assets/main.scss';
 
 function App() {
+  const tags = [
+    { name: 'lorem' },
+    { name: 'ipsum' },
+    { name: 'XD' },
+  ];
+
   return (
-    <Card />
+    <Card
+      title="Dolor eu deserunt commodo pariatur excepteur sit ipsum fugiat."
+      description="Ut qui laboris quis aliqua amet incididunt esse sint sit occaecat. Amet in elit aliquip dolore aliquip cillum dolore voluptate ex dolore. Id anim labore deserunt non et. Deserunt cupidatat dolor aute cupidatat reprehenderit. Ipsum labore id aute ipsum aute culpa sunt amet exercitation esse nostrud aute sit minim. Nulla nisi nisi cupidatat esse laboris."
+      tags={tags}
+    />
   );
 }
 
--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import Chip from '../chip';
 
-const Card = () => {
+type Tag = {
+  name: string
+}
+
+type TaskProps = {
+  title: string
+  description: string
+  tags: Tag[]
+}
+
+const Card = (task: TaskProps) => {
+  const { title, description, tags } = task;
+
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      Card
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">{title}</div>
+        <p className="text-gray-700 text-base">
+          {description}
+        </p>
+      </div>
+      { 
+        tags && tags.length && <div className="px-6 py-4">
+          { tags.map((tag: Tag )=> {
+            return <Chip name={tag.name} />
+          })}
+        </div>
+      }
     </div>
   );
 };
